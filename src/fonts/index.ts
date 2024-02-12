d9499f360c3c5725013466d9f9c94f4834719572
dev-santi <bauzalarrosasantiago@gmail.com>
2024-02-12 20:48:26 -0300

    first commit

---
+++ src/fonts/index.ts
@@ -0,0 +1,4 @@
+import { Oxanium, Raleway } from "next/font/google";
+
+export const oxanium = Oxanium({ subsets: ["latin"] });
+export const ralewar = Raleway({ subsets: ["latin"] });
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,28 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#0088B4",
+                danger: "#F95C19",
+                soft_danger: "#FFE4D9",
+                soft_warning: "#FFEDC9",
+                warning: "#FFAF0F",
+                success: "#00BA77",
+                info: "#00CEC2",
+                white: "#FFFFFF",
+                light_text: "#F0F4F9",
+                dark_text: "#817382",
+                background: "#FFF7E7",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
